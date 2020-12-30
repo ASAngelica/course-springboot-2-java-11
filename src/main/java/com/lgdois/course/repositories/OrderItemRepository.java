9880204bd2881147ab77318ecdf2f9b0a8ad67a5
ASAngelica <asa196549@gmail.com>
2020-12-30 15:43:39 -0300

    Exception handling - delete

--- src/main/java/com/lgdois/course/repositories/OrderItemRepository.java
+++ src/main/java/com/lgdois/course/repositories/OrderItemRepository.java
@@ -2,7 +2,6 @@ package com.lgdois.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.lgdois.course.entities.Order;
 import com.lgdois.course.entities.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
